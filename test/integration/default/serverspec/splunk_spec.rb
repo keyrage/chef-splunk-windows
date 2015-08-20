330208fd578d81f2ad41d39f677241ceb50e0394
Jared King <jared.king@biola.edu>
2015-08-19 22:08:13 -0700

    Added basic tests

---
+++ test/integration/default/serverspec/splunk_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe package('UniversalForwarder') do
+  it { should be_installed }
+end
+
+describe service('SplunkForwarder') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
