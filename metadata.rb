27be6a602773d3b2180c87395d4af92742954e30
Craig Beetlestone <cbeetlestone@gmail.com>
2018-07-31 20:03:23 +1200

    Setting up for testing with Chef 14

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,15 @@
 name             'chef-splunk-windows'
 maintainer       'Craig Beetlestome'
 maintainer_email 'cbeetlestone@gmail.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures the Splunk Universal Forwarder on Windows servers'
 long_description 'Installs and configures the Splunk Universal Forwarder on Windows servers'
 source_url       'https://github.com/biola/chef-splunk-windows'
 issues_url       'https://github.com/biola/chef-splunk-windows/issues'
 version          '0.5.1'
+
+supports windows
+chef_version '~> 14'
 
 depends          'chef-vault', '>= 1.0'
 depends          'windows', '~> 1.34'
